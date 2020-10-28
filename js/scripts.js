513a8a9522c0ce13d7e67ae9b6de639fd5aceba3
pro100Sardor <sardorbek.juraqulov@gmail.com>
2020-10-28 18:39:29 +0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+var euroInUzs = 12285.78;
+var yuanInUzs = 11457.51;
+var rublInUzs = 136.11;
+var liraInUzs = 1301.33;
+var funtInUzs = 13509.28;
+var dollarInUzs = 10359.88;
+
+var currencyConverterForm = document.querySelector('#currencyConverterForm');
+
+currencyConverterForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+
+  var currencyUnit = currencyConverterForm.querySelector('#currencyUnit').value.trim();
+
+  var currencyValue = parseFloat(currencyConverterForm.querySelector('#currencyValue').value.trim(), 10);
+
+  var currencyInSum;
+
+  if (currencyUnit === "euro") {
+    currencyInSum = currencyValue * euroInUzs;
+  } else if (currencyUnit === "yuan") {
+    currencyInSum = currencyValue * yuanInUzs;
+  } else if (currencyUnit === "rubl") {
+    currencyInSum = currencyValue * rublInUzs;
+  } else if (currencyUnit === "lira") {
+    currencyInSum = currencyValue * liraInUzs;
+  } else if (currencyUnit === "funt") {
+    currencyInSum = currencyValue * funtInUzs;
+  } else if (currencyUnit === "dollar") {
+    currencyInSum = currencyValue * dollarInUzs;
+  } else {
+    currencyInSum = "Bunday pul birligi topilmadi"
+  }
+
+  var convertResult = document.querySelector('#currencyInSum');
+  convertResult.textContent = currencyInSum;
+});
